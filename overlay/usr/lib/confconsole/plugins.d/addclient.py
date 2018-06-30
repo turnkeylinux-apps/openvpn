ceb61a663faf8ccfcdc655dfafa03cfe3fda79b1
Yevhenii Khvorostian <qq7dev@gmail.com>
2018-06-30 23:24:16 +0300

    update addclient.py to show download URL when done

--- overlay/usr/lib/confconsole/plugins.d/addclient.py
+++ overlay/usr/lib/confconsole/plugins.d/addclient.py
@@ -12,14 +12,14 @@ def run():
 
         rte, email = console.inputbox(TITLE, "Enter email")
   
-        if rte == 0 rty == 0:
+        if rte == 0 and rty == 0:
             proc = Popen(["openvpn-addclient", name, email], stderr=PIPE)
             _, out = proc.communicate()
             returncode = proc.returncode
-            if returncode:
-                console.msgbox(TITLE, '{} ({})'.format(out, name))
-                continue
-
-            break
+            if returncode == 0:
+                console.msgbox(TITLE, check_output(["/var/www/openvpn/bin/addprofile", name])) 
+            elif returncode == 1:
+		        console.msgbox(TITLE, '{} ({})'.format(out, name))
+	    break
         else:
-           break    
+           break
