d7dafa2cbfad91254a1ce78d234fee0136f037bd
Stefan Davis <stefan@turnkeylinux.org>
2020-10-12 16:56:52 +1100

    Update confconsole plugins

--- overlay/usr/lib/confconsole/plugins.d/addclient.py
+++ overlay/usr/lib/confconsole/plugins.d/addclient.py
@@ -8,16 +8,16 @@ TITLE = "Add client"
 def run():
     while True:
         rty, name = console.inputbox(TITLE, "Enter name")
-        if rty == 1: break
+        if rty == 'cancel': break
 
         rte, email = console.inputbox(TITLE, "Enter email")
-  
-        if rte == 0 and rty == 0:
+
+        if rte == 'ok' and rty == 'ok':
             proc = Popen(["openvpn-addclient", name, email], stderr=PIPE)
             _, out = proc.communicate()
             returncode = proc.returncode
             if returncode == 0:
-                console.msgbox(TITLE, check_output(["/var/www/openvpn/bin/addprofile", name])) 
+                console.msgbox(TITLE, check_output(["/var/www/openvpn/bin/addprofile", name], text=True)) 
             elif returncode == 1:
                         console.msgbox(TITLE, '{} ({})'.format(out, name))
             break
